05da0cc204bfee48e4387f21e7ad24178386353d
Ethan Glasby <ethan.a.glasby@gmail.com>
2024-03-30 13:26:03 -0400

    Fix mui and add a tiny bit of table

--- apps/frontend/src/components/Requests.tsx
+++ apps/frontend/src/components/Requests.tsx
@@ -1,7 +1,27 @@
+import {
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import React from "react";
+
 function Requests() {
   return (
     <div>
       <p>not made yet</p>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 10 }} aria-label="Requests Table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Delivery Type</TableCell>
+              <TableCell>Request Number</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
