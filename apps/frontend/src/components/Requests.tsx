5c003bdd0c2daa5808a99247e83cdf2718ee1b5c
Ethan Glasby <ethan.a.glasby@gmail.com>
2024-03-30 13:51:30 -0400

    Fix mui and add a tiny bit of table

--- apps/frontend/src/components/Requests.tsx
+++ apps/frontend/src/components/Requests.tsx
@@ -1,6 +1,7 @@
 import {
   Paper,
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableHead,
@@ -8,18 +9,39 @@ import {
 } from "@mui/material";
 import React from "react";
 
+//This is in here as a placeholder for the actual data
+/*
+const rows = [
+  12345,
+  "Jim Brown",
+  "Jane Brown",
+  123,
+  "violets",
+  "Get well soon! See you after work!",
+];
+*/
+
 function Requests() {
   return (
     <div>
       <p>not made yet</p>
+      /*mostly copied from https://mui.com/material-ui/react-table/ */
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 10 }} aria-label="Requests Table">
+        <Table sx={{ minWidth: 10 }} aria-label="FLowers Requests Table">
           <TableHead>
             <TableRow>
               <TableCell>Delivery Type</TableCell>
               <TableCell>Request Number</TableCell>
+              <TableCell>Sender Name</TableCell>
+              <TableCell>Patient Name</TableCell>
+              <TableCell>Room Number</TableCell>
+              <TableCell>Flower Type</TableCell>
+              <TableCell>Message</TableCell>
             </TableRow>
           </TableHead>
+          <TableBody>
+            /*gets really messy without the actual data structure*/
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
