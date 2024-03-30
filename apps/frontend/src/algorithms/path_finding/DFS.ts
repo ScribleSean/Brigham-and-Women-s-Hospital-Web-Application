7949326654bc1ba35442043f3fa9c801c64b5f65
Jose Manuel Perez <josemanuel2000@gmail.com>
2024-03-29 20:51:17 -0400

    Update DFS.ts

--- apps/frontend/src/algorithms/path_finding/DFS.ts
+++ apps/frontend/src/algorithms/path_finding/DFS.ts
@@ -3,7 +3,14 @@ import {Edge, Graph, Node, Path, Queue, Stack} from "../DataStructures.ts";
 import {IPathFinder} from "../PathFinder.ts";
 
 export class DFS implements IPathFinder {
-    findPath(graph: Graph, startNode: Node, endNode: Node): Path | undefined {
+
+    graph: Graph;
+
+    constructor(graph: Graph) {
+        this.graph = graph;
+    }
+
+    findPath(startNode: Node, endNode: Node): Path | undefined {
 
     }
 }
