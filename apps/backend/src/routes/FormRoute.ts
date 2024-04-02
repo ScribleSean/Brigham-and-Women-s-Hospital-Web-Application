27118ac54595f480d571fd3f1702a6e80286daab
Lorenzo-Cassano <lvcassano@wpi.edu>
2024-04-01 17:02:18 -0700

    Requests.tsx -- Request1

--- apps/backend/src/routes/FormRoute.ts
+++ apps/backend/src/routes/FormRoute.ts
@@ -4,6 +4,20 @@ import { Form } from "common/src/Form.ts";
 
 const router: Router = express.Router();
 
+router.post("/", async function (req, res) {
+  const form: Form = req.body;
+
+  try {
+    await PrismaClient.form.create({
+      data: form,
+    });
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "error" });
+  }
+});
+
 router.get("/", async function (req: Request, res: Response): Promise<void> {
   const form: Form[] = await PrismaClient.form.findMany();
   res.json(form);
