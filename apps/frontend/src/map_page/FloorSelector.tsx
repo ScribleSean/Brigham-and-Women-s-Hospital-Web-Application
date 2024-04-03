69a853a520734f774e9a62bacb86efb5fc522130
gusmontana <gmt015@gmail.com>
2024-04-02 22:11:24 -0400

    made buttons work

--- apps/frontend/src/map_page/FloorSelector.tsx
+++ apps/frontend/src/map_page/FloorSelector.tsx
@@ -1,115 +1,51 @@
 import { FloorSelectorProps } from "./types/map_page_types.ts";
-import React, { CSSProperties } from "react";
+import React from "react";
 import { FloorType } from "../../../backend/src/algorithms/DataStructures.ts";
 import { Button } from "@mui/material";
+import "../styles/FloorSelector.css";
 
 export function FloorSelector(props: FloorSelectorProps): React.JSX.Element {
   const { updateFloor } = props;
 
-  const formStyle: CSSProperties = {
-    paddingTop: "5%",
-    paddingBottom: "3%",
-    paddingLeft: "8%",
-    paddingRight: "8%",
-    position: "relative",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "5%",
-    width: "100%",
-  };
-
-  const formatButtonText = (text: string) => {
-    //console.log(text.charAt(0).toUpperCase() + text.slice(1).toLowerCase());
-    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-  };
-
   return (
-    <form style={formStyle}>
-      <Button
-        sx={{
-          borderColor: "black",
-          backgroundColor: "white",
-          font: "Inter",
-          color: "black",
-          boxShadow: 3,
-          width: 200,
-        }}
-        type="button"
-        className="btn-check"
-        id="btn-check-l2"
-        onClick={() => updateFloor(FloorType.L2)}
-      >
-        L2
-      </Button>
-
-      <Button
-        sx={{
-          borderColor: "black",
-          backgroundColor: "white",
-          font: "Inter",
-          color: "black",
-          boxShadow: 3,
-          width: 200,
-        }}
-        type="button"
-        className="btn-check"
-        id="btn-check-l1"
-        onClick={() => updateFloor(FloorType.L1)}
-      >
-        L1
-      </Button>
-
-      <Button
-        sx={{
-          borderColor: "black",
-          backgroundColor: "white",
-          font: "Inter",
-          color: "black",
-          boxShadow: 3,
-          width: 200,
-        }}
-        type="button"
-        className="btn-check"
-        id="btn-check-first"
-        onClick={() => updateFloor(FloorType.first)}
-      >
-        {formatButtonText("Floor 1")}
-      </Button>
-
-      <Button
-        sx={{
-          borderColor: "black",
-          backgroundColor: "white",
-          font: "Inter",
-          color: "black",
-          boxShadow: 3,
-          width: 200,
-        }}
-        type="button"
-        className="btn-check"
-        id="btn-check-second"
-        onClick={() => updateFloor(FloorType.second)}
-      >
-        {formatButtonText("Floor 2")}
-      </Button>
-
-      <Button
-        sx={{
-          borderColor: "black",
-          backgroundColor: "white",
-          font: "Inter",
-          color: "black",
-          boxShadow: 3,
-          width: 200,
-        }}
-        type="button"
-        className="btn-check"
-        id="btn-check-third"
-        onClick={() => updateFloor(FloorType.third)}
-      >
-        {formatButtonText("Floor 3")}
-      </Button>
-    </form>
+    <>
+      <div className={"btn-container"}>
+        <Button
+          onClick={() => updateFloor(FloorType.L1)}
+          variant={"contained"}
+          className={"floor-btn"}
+        >
+          L1
+        </Button>
+        <Button
+          onClick={() => updateFloor(FloorType.L2)}
+          variant={"contained"}
+          className={"floor-btn"}
+        >
+          L2
+        </Button>
+        <Button
+          onClick={() => updateFloor(FloorType.first)}
+          variant={"contained"}
+          className={"floor-btn"}
+        >
+          Floor 1
+        </Button>
+        <Button
+          onClick={() => updateFloor(FloorType.second)}
+          variant={"contained"}
+          className={"floor-btn"}
+        >
+          Floor 2
+        </Button>
+        <Button
+          onClick={() => updateFloor(FloorType.third)}
+          variant={"contained"}
+          className={"floor-btn"}
+        >
+          Floor 3
+        </Button>
+      </div>
+    </>
   );
 }
