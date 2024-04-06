d43cfc83c9bd5d51f86f20bea26131edc30643ae
gabriel_o <olafssongabe@gmail.com>
2024-04-06 14:29:28 -0400

    changed nodes to node,

---
+++ packages/common/src/form.ts
@@ -0,0 +1,9 @@
+// this will be gone once we rework the Active Request page
+
+export type Form = {
+  receiverName: string;
+  message: string;
+  senderName: string;
+  flowerType: string;
+  roomNumber: number;
+};
