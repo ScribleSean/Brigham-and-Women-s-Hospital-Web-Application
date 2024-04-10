b208726d443ccf1638ed864c8e1929cba850b310
Christian Consiglio <fastjr2002@gmail.com>
2024-04-10 17:47:58 -0400

    Added useState stuff at the bottom of forms when submitted

--- apps/frontend/src/routes/service_request_routes/RoomScheduling.tsx
+++ apps/frontend/src/routes/service_request_routes/RoomScheduling.tsx
@@ -1,13 +1,15 @@
 import {
-  Button,
-  FormControl,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-  Snackbar,
-  TextField,
+    Button,
+    FormControl,
+    InputAdornment,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+    Snackbar,
+    Table, TableBody, TableCell,
+    TableContainer, TableHead, TableRow,
+    TextField,
 } from "@mui/material";
 import "../../styles/RoomScheduling.css";
 import React, { useEffect, useState } from "react";
@@ -107,156 +109,198 @@ function RoomScheduling() {
         anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
       />
 
-      <div className={"san-div col-10"}>
-        <form className={"request-form"} onSubmit={handleSubmit}>
-          <h1>Room Request</h1>
-          <h5>Ethan and Lorenzo</h5>
-          <br />
-          <div className={`${styles.twoInputRow}`}>
-            <TextField
-              label={"Employee Name"}
-              variant={"filled"}
-              id={"employeeName"}
-              sx={{
-                my: "1%",
-                width: "49.5%",
-                marginRight: "1%",
-              }}
-              onChange={handleTextFieldChange}
-              value={formData.employeeName}
-              required
-            />
-            <TextField
-              label={"Location"}
-              variant={"filled"}
-              id={"location"}
-              sx={{
-                my: "1%",
-                width: "49.5%",
-              }}
-              onChange={handleTextFieldChange}
-              value={formData.location}
-              required
-            />
-          </div>
-          <br />
+        <div className={"san-div col-10"}>
+            <form className={"request-form"} onSubmit={handleSubmit}>
+                <h1>Room Request</h1>
+                <h5>Ethan and Lorenzo</h5>
+                <br/>
+                <div className={`${styles.twoInputRow}`}>
+                    <TextField
+                        label={"Employee Name"}
+                        variant={"filled"}
+                        id={"employeeName"}
+                        sx={{
+                            my: "1%",
+                            width: "49.5%",
+                            marginRight: "1%",
+                        }}
+                        onChange={handleTextFieldChange}
+                        value={formData.employeeName}
+                        required
+                    />
+                    <TextField
+                        label={"Location"}
+                        variant={"filled"}
+                        id={"location"}
+                        sx={{
+                            my: "1%",
+                            width: "49.5%",
+                        }}
+                        onChange={handleTextFieldChange}
+                        value={formData.location}
+                        required
+                    />
+                </div>
+                <br/>
 
-          <div className={"two-input-row-container"}>
-            <FormControl
-              variant={"filled"}
-              sx={{ width: "49%", marginRight: "1%", my: "1%" }}
-              required
-            >
-              <TextField
-                label={"Start Time"}
-                variant={"filled"}
-                id={"startTime"}
-                sx={{ my: "1%" }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position={"start"}></InputAdornment>
-                  ),
-                }}
-                type={"datetime-local"}
-                onChange={handleTextFieldChange}
-                value={formData.startTime}
-                InputLabelProps={{ shrink: true }}
-                required
-              />
-            </FormControl>
-            <FormControl
-              variant={"filled"}
-              sx={{ width: "49%", marginRight: "1%", my: "1%" }}
-              required
-            >
-              <TextField
-                label={"Duration"}
-                variant={"filled"}
-                id={"duration"}
-                type={"number"}
-                sx={{ my: "1%" }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position={"end"}>hrs</InputAdornment>
-                  ),
-                }}
-                onChange={handleTextFieldChange}
-                value={formData.duration}
-                required
-              />
-            </FormControl>
-          </div>
+                <div className={"two-input-row-container"}>
+                    <FormControl
+                        variant={"filled"}
+                        sx={{width: "49%", marginRight: "1%", my: "1%"}}
+                        required
+                    >
+                        <TextField
+                            label={"Start Time"}
+                            variant={"filled"}
+                            id={"startTime"}
+                            sx={{my: "1%"}}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position={"start"}></InputAdornment>
+                                ),
+                            }}
+                            type={"datetime-local"}
+                            onChange={handleTextFieldChange}
+                            value={formData.startTime}
+                            InputLabelProps={{shrink: true}}
+                            required
+                        />
+                    </FormControl>
+                    <FormControl
+                        variant={"filled"}
+                        sx={{width: "49%", marginRight: "1%", my: "1%"}}
+                        required
+                    >
+                        <TextField
+                            label={"Duration"}
+                            variant={"filled"}
+                            id={"duration"}
+                            type={"number"}
+                            sx={{my: "1%"}}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position={"end"}>hrs</InputAdornment>
+                                ),
+                            }}
+                            onChange={handleTextFieldChange}
+                            value={formData.duration}
+                            required
+                        />
+                    </FormControl>
+                </div>
 
-          <div className={"two-input-row-container"}>
-            <FormControl
-              variant={"filled"}
-              sx={{ width: "49%", marginRight: "1%", my: "1%" }}
-              required
-            >
-              <InputLabel id={"priority"}>Priority</InputLabel>
-              <Select
-                id={"priority"}
-                onChange={(e) => handleSelectChange(e, "priority")}
-                value={formData.priority}
-              >
-                <MenuItem value={"Low"}>Low</MenuItem>
-                <MenuItem value={"Medium"}>Medium</MenuItem>
-                <MenuItem value={"High"}>High</MenuItem>
-                <MenuItem value={"Emergency"}>Emergency</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl
-              variant={"filled"}
-              sx={{ width: "49%", marginLeft: "1%", my: "1%" }}
-              required
-            >
-              <InputLabel id={"status"}>Status</InputLabel>
-              <Select
-                id={"status"}
-                onChange={(e) => handleSelectChange(e, "status")}
-                value={formData.status}
-              >
-                <MenuItem value={"Unassigned"}>Unassigned</MenuItem>
-                <MenuItem value={"Assigned"}>Assigned</MenuItem>
-                <MenuItem value={"In Progress"}>In Progress</MenuItem>
-                <MenuItem value={"Closed"}>Closed</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-          <br />
-          <div className={"button-container"}>
-            <Button
-              variant={"outlined"}
-              color={"error"}
-              sx={{
-                width: "25%",
-              }}
-              onClick={() => {
-                setFormData({
-                  employeeName: "", //text box
-                  priority: "", //radio buttons
-                  location: "", //text box
-                  startTime: "", //datetime local
-                  duration: 0, //numbers only
-                  status: "", //radio buttons
-                });
-              }}
-            >
-              Clear
-            </Button>
-            <Button
-              variant={"contained"}
-              type={"submit"}
-              sx={{
-                width: "25%",
-                backgroundColor: "#012d5a",
-              }}
-            >
-              Submit
-            </Button>
-          </div>
-        </form>
-      </div>
+                <div className={"two-input-row-container"}>
+                    <FormControl
+                        variant={"filled"}
+                        sx={{width: "49%", marginRight: "1%", my: "1%"}}
+                        required
+                    >
+                        <InputLabel id={"priority"}>Priority</InputLabel>
+                        <Select
+                            id={"priority"}
+                            onChange={(e) => handleSelectChange(e, "priority")}
+                            value={formData.priority}
+                        >
+                            <MenuItem value={"Low"}>Low</MenuItem>
+                            <MenuItem value={"Medium"}>Medium</MenuItem>
+                            <MenuItem value={"High"}>High</MenuItem>
+                            <MenuItem value={"Emergency"}>Emergency</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl
+                        variant={"filled"}
+                        sx={{width: "49%", marginLeft: "1%", my: "1%"}}
+                        required
+                    >
+                        <InputLabel id={"status"}>Status</InputLabel>
+                        <Select
+                            id={"status"}
+                            onChange={(e) => handleSelectChange(e, "status")}
+                            value={formData.status}
+                        >
+                            <MenuItem value={"Unassigned"}>Unassigned</MenuItem>
+                            <MenuItem value={"Assigned"}>Assigned</MenuItem>
+                            <MenuItem value={"In Progress"}>In Progress</MenuItem>
+                            <MenuItem value={"Closed"}>Closed</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+                <br/>
+                <div className={"button-container"}>
+                    <Button
+                        variant={"outlined"}
+                        color={"error"}
+                        sx={{
+                            width: "25%",
+                        }}
+                        onClick={() => {
+                            setFormData({
+                                employeeName: "", //text box
+                                priority: "", //radio buttons
+                                location: "", //text box
+                                startTime: "", //datetime local
+                                duration: 0, //numbers only
+                                status: "", //radio buttons
+                            });
+                        }}
+                    >
+                        Clear
+                    </Button>
+                    <Button
+                        variant={"contained"}
+                        type={"submit"}
+                        sx={{
+                            width: "25%",
+                            backgroundColor: "#012d5a",
+                        }}
+                    >
+                        Submit
+                    </Button>
+                </div>
+            </form>
+            <br/>
+            <div>
+                <h2>Active Requests</h2>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    <b>Employee Name</b>
+                                </TableCell>
+                                <TableCell>
+                                    <b>Location</b>
+                                </TableCell>
+                                <TableCell>
+                                    <b>Start Time</b>
+                                </TableCell>
+                                <TableCell>
+                                    <b>Duration</b>
+                                </TableCell>
+                                <TableCell>
+                                    <b>Priority</b>
+                                </TableCell>
+                                <TableCell>
+                                    <b>Status</b>
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {submittedRequests.map((request) => (
+                                <TableRow>
+                                    <TableCell>{request.employeeName}</TableCell>
+                                    <TableCell>{request.location}</TableCell>
+                                    <TableCell>{request.startTime}</TableCell>
+                                    <TableCell>{request.duration}</TableCell>
+                                    <TableCell>{request.priority}</TableCell>
+                                    <TableCell>{request.status}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
     </>
   );
 }
