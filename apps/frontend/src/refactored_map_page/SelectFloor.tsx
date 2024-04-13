3468581fca70959c0079dd716fba03c5461efcdb
tahutzley <thutzley1@gmail.com>
2024-04-12 21:46:06 -0400

    Styled floor buttons

--- apps/frontend/src/refactored_map_page/SelectFloor.tsx
+++ apps/frontend/src/refactored_map_page/SelectFloor.tsx
@@ -5,27 +5,40 @@ import "../map_page/FloorSelector.css";
 import { useMapContext } from "./MapContext.ts";
 
 export function FloorSelector(): React.JSX.Element {
-  const { setCurrentFloor } = useMapContext();
+  const { currentFloor, setCurrentFloor } = useMapContext();
+
+  const handleOnClick = (floor: FloorType) => {
+    setCurrentFloor(floor);
+  };
 
   return (
     <>
       <div className={"btn-container"}>
         <div>
           <Button
-            onClick={() => setCurrentFloor(FloorType.L2)}
+            onClick={() => handleOnClick(FloorType.L2)}
             variant={"contained"}
+            disableRipple
             sx={{
-              backgroundColor: "blue",
-              height: "6vh",
+              backgroundColor:
+                currentFloor === FloorType.L2 ? "#012D5A" : "white",
+              ":hover": {
+                backgroundColor:
+                  currentFloor === FloorType.L2 ? "#012D5A" : "#e0e0e0",
+              },
+              height: currentFloor === FloorType.L2 ? "6.5vh" : "5vh",
               width: "10%",
-              color: "#012D5A",
+              color: currentFloor === FloorType.L2 ? "white" : "#012D5A",
               fontFamily: "inter",
               fontWeight: "bold",
               fontSize: "1.2rem",
               textTransform: "capitalize",
-              boxShadow: 3,
+              boxShadow:
+                currentFloor === FloorType.L2
+                  ? "inset 0 0.25rem 0.25rem rgba(0, 0, 0, 0.25)"
+                  : 3,
               borderRadius: 2,
-              transition: "width 0.3s ease",
+              transition: "height 0.15s ease",
             }}
           >
             L2
@@ -33,77 +46,113 @@ export function FloorSelector(): React.JSX.Element {
         </div>
 
         <Button
-          onClick={() => setCurrentFloor(FloorType.L1)}
+          onClick={() => handleOnClick(FloorType.L1)}
           variant={"contained"}
+          disableRipple
           sx={{
-            backgroundColor: "blue",
-            height: "6vh",
+            backgroundColor:
+              currentFloor === FloorType.L1 ? "#012D5A" : "white",
+            ":hover": {
+              backgroundColor:
+                currentFloor === FloorType.L1 ? "#012D5A" : "#e0e0e0",
+            },
+            height: currentFloor === FloorType.L1 ? "6.5vh" : "5vh",
             width: "10%",
-            color: "#012D5A",
+            color: currentFloor === FloorType.L1 ? "white" : "#012D5A",
             fontFamily: "inter",
             fontWeight: "bold",
             fontSize: "1.2rem",
             textTransform: "capitalize",
-            boxShadow: 3,
+            boxShadow:
+              currentFloor === FloorType.L1
+                ? "inset 0 0.25rem 0.25rem rgba(0, 0, 0, 0.25)"
+                : 3,
             borderRadius: 2,
-            transition: "width 0.3s ease",
+            transition: "height 0.05s ease",
           }}
         >
           L1
         </Button>
         <Button
-          onClick={() => setCurrentFloor(FloorType.first)}
+          onClick={() => handleOnClick(FloorType.first)}
           variant={"contained"}
+          disableRipple
           sx={{
-            backgroundColor: "blue",
-            height: "6vh",
+            backgroundColor:
+              currentFloor === FloorType.first ? "#012D5A" : "white",
+            ":hover": {
+              backgroundColor:
+                currentFloor === FloorType.first ? "#012D5A" : "#e0e0e0",
+            },
+            height: currentFloor === FloorType.first ? "6.5vh" : "5vh",
             width: "10%",
-            color: "#012D5A",
+            color: currentFloor === FloorType.first ? "white" : "#012D5A",
             fontFamily: "inter",
             fontWeight: "bold",
             fontSize: "1.2rem",
             textTransform: "capitalize",
-            boxShadow: 3,
+            boxShadow:
+              currentFloor === FloorType.first
+                ? "inset 0 0.25rem 0.25rem rgba(0, 0, 0, 0.25)"
+                : 3,
             borderRadius: 2,
-            transition: "width 0.3s ease",
+            transition: "height 0.05s ease",
           }}
         >
           1
         </Button>
         <Button
-          onClick={() => setCurrentFloor(FloorType.second)}
+          onClick={() => handleOnClick(FloorType.second)}
           variant={"contained"}
+          disableRipple
           sx={{
-            backgroundColor: "blue",
-            height: "6vh",
+            backgroundColor:
+              currentFloor === FloorType.second ? "#012D5A" : "white",
+            ":hover": {
+              backgroundColor:
+                currentFloor === FloorType.second ? "#012D5A" : "#e0e0e0",
+            },
+            height: currentFloor === FloorType.second ? "6.5vh" : "5vh",
             width: "10%",
-            color: "#012D5A",
+            color: currentFloor === FloorType.second ? "white" : "#012D5A",
             fontFamily: "inter",
             fontWeight: "bold",
             fontSize: "1.2rem",
             textTransform: "capitalize",
-            boxShadow: 3,
+            boxShadow:
+              currentFloor === FloorType.second
+                ? "inset 0 0.25rem 0.25rem rgba(0, 0, 0, 0.25)"
+                : 3,
             borderRadius: 2,
-            transition: "width 0.3s ease",
+            transition: "height 0.05s ease",
           }}
         >
           2
         </Button>
         <Button
-          onClick={() => setCurrentFloor(FloorType.third)}
+          onClick={() => handleOnClick(FloorType.third)}
           variant={"contained"}
+          disableRipple
           sx={{
-            backgroundColor: "blue",
-            height: "6vh",
+            backgroundColor:
+              currentFloor === FloorType.third ? "#012D5A" : "white",
+            ":hover": {
+              backgroundColor:
+                currentFloor === FloorType.third ? "#012D5A" : "#e0e0e0",
+            },
+            height: currentFloor === FloorType.third ? "6.5vh" : "5vh",
             width: "10%",
-            color: "#012D5A",
+            color: currentFloor === FloorType.third ? "white" : "#012D5A",
             fontFamily: "inter",
             fontWeight: "bold",
             fontSize: "1.2rem",
             textTransform: "capitalize",
-            boxShadow: 3,
+            boxShadow:
+              currentFloor === FloorType.third
+                ? "inset 0 0.25rem 0.25rem rgba(0, 0, 0, 0.25)"
+                : 3,
             borderRadius: 2,
-            transition: "width 0.3s ease",
+            transition: "height 0.05s ease",
           }}
         >
           3
