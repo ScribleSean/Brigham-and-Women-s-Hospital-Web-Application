1e2d5425eb51aaeed1b10f01824bd58463c1df96
SofiaXie <sofalofax@gmail.com>
2024-04-16 16:32:46 -0400

    Finshed Hero Page, without animations

--- apps/frontend/src/routes/HeroPage.tsx
+++ apps/frontend/src/routes/HeroPage.tsx
@@ -1,11 +1,40 @@
 import "../styles/HeroPage.css";
+// import 'animate.css';
+// import {Button} from "@mui/material";
+// @import "~animate.css/animate.css";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 
 export default function heroPage() {
   return (
     <div className={"image-area"}>
-      <div className={"gradient"}>
-        <div className={"hero-text"}>
-          <h1> Welcome to Brigham and Women's Hospital </h1>
+      <div className={"gradient row"}>
+        {/*Text*/}
+        <div className={"col-8"}>
+          <div className={"hero-text"}>
+            {" "}
+            Welcome to Brigham and Women's Hospital{" "}
+          </div>
+          <div className={"hero-text-p"}>
+            <p>
+              {" "}
+              Helping our patients and their families get back to what matters
+              most.
+            </p>
+          </div>
+        </div>
+
+        {/*map function*/}
+        <div className={"col-4 d-flex justify-content-end"}>
+          <div className={"toMap"}>
+            <button className={"button-class"}> Go To Map </button>
+            <ArrowCircleRightIcon
+              sx={{ color: "#ffffff", fontSize: 48 }}
+            ></ArrowCircleRightIcon>
+            {/*<div className={"arrow"} ></div>*/}
+          </div>
+          {/*<div className={"d-flex justify-content-start"}>*/}
+          {/*    <p> Staff Member? Log In</p>*/}
+          {/*</div>*/}
         </div>
       </div>
     </div>
