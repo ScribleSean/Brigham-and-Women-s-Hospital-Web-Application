0613e8cd3a6fc821f3cc57b2ef3641b7e713ed3d
SofiaXie <sofalofax@gmail.com>
2024-04-22 00:18:52 -0400

    I will never vertically algin agian

--- apps/frontend/src/routes/HeroPage.tsx
+++ apps/frontend/src/routes/HeroPage.tsx
@@ -42,80 +42,106 @@ function HeroPage() {
 
   return (
     <div className={"image-area"}>
-      <div className={"gradient row "}>
+      <div className={"gradient row justify-content-around m-0"}>
         {/*Hospital Text*/}
-        <div className={"col-8 border"}>
+        <div className={"col-8 p-0 border"}>
           <div className={"header borders"}>
             Welcome to Brigham and Women's Hospital
           </div>
 
+          <div className={"space borders"}></div>
+
           {/*Carousel*/}
-          <div className="carousel hero-text-p borders">
+          <div className="carousel subtitle borders">
             <div>
-              <p>
+              <span>
                 Helping our patients and their families get back to what matters
                 most.
-              </p>
-              <p>
+              </span>
+              <span>
                 Excellence in medical research and patient care, our commitment
                 continues.
-              </p>
-              <p>
+              </span>
+              <span>
                 Dedicated to a century of leadership in healthcare and patient
                 service.
-              </p>
-              <p>
+              </span>
+              <span>
                 Leading the way in comprehensive healthcare, where every patient
                 is family.
-              </p>
-              <p>
+              </span>
+              <span>
                 Together in health, every step of the way—because family
                 matters.
-              </p>
+              </span>
+              <span>
+                Helping our patients and their families get back to what matters
+                most.
+              </span>
+              {/*<p>*/}
+              {/*    Helping our patients and their families get back to what matters most. <br>*/}
+
+              {/*    Excellence in medical research and patient care, our commitment continues. <br>*/}
+
+              {/*    Dedicated to a century of leadership in healthcare and patient service. <br>*/}
+
+              {/*    Leading the way in comprehensive healthcare, where every patient is family. <br>*/}
+
+              {/*    Together in health, every step of the way—because family matters. <br>*/}
+
+              {/*</p>*/}
             </div>
           </div>
         </div>
 
-        {/*Go to map*/}
-        <div className={"col-4 d-flex justify-content-end paragraph borders"}>
-          <a
-            href={`${isAuthenticated ? "/dashboard" : "/public-map"}`}
-            id={"toMapClump"}
-            className={
-              "toMap animate__animated animate__slower animate__headShake animate__infinite"
-            }
-            // @ts-expect-error works
-            onClick={addAnimationClass}
-          >
-            <button className={"button-class"}> Go To Map</button>
-            <ArrowCircleRightIcon
-              sx={{
-                color: "#ffffff",
-                width: "1.75em",
-                height: "1.75em",
+        {/*Right hand Column*/}
 
-                marginBottom: 1,
-              }}
-            ></ArrowCircleRightIcon>
-            {/*<div className={"arrow"} ></div>*/}
-          </a>
-        </div>
+        <div
+          className={
+            " col-4 p-0 d-flex flex-column justify-content-between borders"
+          }
+        >
+          {/*room settings display*/}
+          <div className={"boxPad borders"}>
+            <div className={"d-flex justify-content-end"}>
+              <DeviceThermostatIcon sx={{ color: "#ffffff", fontSize: 55 }}>
+                {" "}
+              </DeviceThermostatIcon>
+            </div>
 
-        {/*admin login*/}
-        <div className={"d-flex justify-content-start borders"}>
-          <p> Staff Member? Log In</p>
-        </div>
-
-        {/*room settings display*/}
-        <div className={"borders"}>
-          <div>
-            <DeviceThermostatIcon sx={{ color: "#ffffff", fontSize: 55 }}>
-              {" "}
-            </DeviceThermostatIcon>
+            <div className={"d-flex justify-content-end"}>
+              <img src={HumidityIcon} alt={"Image"} width={"50"} />
+            </div>
           </div>
+          <div className={"space_2"}></div>
+          {/*Go to map*/}
+          <div className={"d-flex justify-content-end paragraph borders"}>
+            <a
+              href={`${isAuthenticated ? "/dashboard" : "/public-map"}`}
+              id={"toMapClump"}
+              className={
+                "toMap animate__animated animate__slower animate__headShake animate__infinite"
+              }
+              // @ts-expect-error works
+              onClick={addAnimationClass}
+            >
+              <button className={"button-class"}> Go To Map</button>
+              <ArrowCircleRightIcon
+                sx={{
+                  color: "#ffffff",
+                  width: "1.75em",
+                  height: "1.75em",
 
-          <div>
-            <img src={HumidityIcon} alt={"Image"} width={"50"} />
+                  marginBottom: 1,
+                }}
+              ></ArrowCircleRightIcon>
+              {/*<div className={"arrow"} ></div>*/}
+            </a>
+          </div>
+          <div className={"space_2"}></div>s{/*admin login*/}
+          <div className={"d-flex justify-content-center paragraph borders"}>
+            <p className={"boxPad"}> Staff Member? Log In</p>
+            {/*<p>Log In</p>*/}
           </div>
         </div>
       </div>
