5ea8b3488b56318826abf19522f444820e7dad76
gabrielolafs <olafssongabe@gmail.com>
2024-04-22 15:30:14 -0400

    made the dropdowns directory in the routes folder. I am not messing with the csv pages anymore cause they rely on each other too much and everything with the csv pages break when i refactor. I also never even touched algos routes. Deleted a route with litterally nothing in it

--- apps/backend/src/routes/dropdowns/employeeEmailRouter.ts
+++ apps/backend/src/routes/dropdowns/employeeEmailRouter.ts
@@ -1,6 +1,6 @@
 // fetch employee emails from employee table
 import express, { Router } from "express";
-import PrismaClient from "../bin/database-connection.ts";
+import PrismaClient from "../../bin/database-connection.ts";
 
 const router: Router = express.Router();
 
--- apps/backend/src/routes/dropdowns/room-name-fetch.ts
+++ apps/backend/src/routes/dropdowns/room-name-fetch.ts
@@ -1,6 +1,6 @@
 // fetch room location from Node table
 import express, { Router } from "express";
-import PrismaClient from "../bin/database-connection.ts";
+import PrismaClient from "../../bin/database-connection.ts";
 
 const router: Router = express.Router();
 
