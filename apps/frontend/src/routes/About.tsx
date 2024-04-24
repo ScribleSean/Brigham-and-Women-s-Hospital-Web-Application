b6f8111b6eb31b56fb59f71a6a879b156d640bfd
Ethan Glasby <ethan.a.glasby@gmail.com>
2024-04-23 22:55:07 -0400

    The carousel works! Now to add images (ew)

--- apps/frontend/src/routes/About.tsx
+++ apps/frontend/src/routes/About.tsx
@@ -10,29 +10,48 @@ function About() {
           <div className={"hero-text"}>About Us</div>
           <div className="carousel hero-text-p">
             <div>
-              <p>Yoooo</p>
+              <p>Sean Arackal</p>
+              <p>Back-End Dev</p>
             </div>
             <div style={{ transform: "translateX(100%)" }}>
-              <p>
-                Helping our patients and their families get back to what matters
-                most.
-              </p>
-              <p>
-                Excellence in medical research and patient care, our commitment
-                continues.
-              </p>
-              <p>
-                Dedicated to a century of leadership in healthcare and patient
-                service.
-              </p>
-              <p>
-                Leading the way in comprehensive healthcare, where every patient
-                is family.
-              </p>
-              <p>
-                Together in health, every step of the way—because family
-                matters.
-              </p>
+              <p>Maddux Berry</p>
+              <p>Project Manager / Front-End Dev</p>
+            </div>
+            <div style={{ transform: "translateX(200%)" }}>
+              <p>Lorenzo Cassano</p>
+              <p>Back-End Dev</p>
+            </div>
+            <div style={{ transform: "translateX(300%)" }}>
+              <p>Peter Czepiel</p>
+              <p>Front-End Dev</p>
+            </div>
+            <div style={{ transform: "translateX(400%)" }}>
+              <p>Christian Consiglio</p>
+              <p>Lead Software Engineer (Front-End)</p>
+            </div>
+            <div style={{ transform: "translateX(500%)" }}>
+              <p>Ethan Glasby</p>
+              <p>Product Owner / Front-End Dev</p>
+            </div>
+            <div style={{ transform: "translateX(600%)" }}>
+              <p>Timothy Hutzley</p>
+              <p>Scrum Master / Front-End Dev</p>
+            </div>
+            <div style={{ transform: "translateX(700%)" }}>
+              <p>José Manuel Pérez Jiménez</p>
+              <p>Assistant Lead Software Engineer (Algorithms)</p>
+            </div>
+            <div style={{ transform: "translateX(800%)" }}>
+              <p>Gustave Montana</p>
+              <p>Front-End Dev</p>
+            </div>
+            <div style={{ transform: "translateX(900%)" }}>
+              <p>Gabriel Olafsson</p>
+              <p>Assistant Lead Software Engineer (Back-End)</p>
+            </div>
+            <div style={{ transform: "translateX(1000%)" }}>
+              <p>Sofia Xie</p>
+              <p>Document Analyst / Front-End Dev</p>
             </div>
           </div>
         </div>
