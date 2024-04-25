5b0246887f61ed1c0a0d13b08810b53bd6fb078d
macethewindu66 <madduxberry31@gmail.com>
2024-04-25 01:46:33 -0400

    Added Strategy Class. Class still needs to be implemented into the MapPage portion of the website

--- packages/common/src/path_finding/algoAbstract.ts
+++ packages/common/src/path_finding/algoAbstract.ts
@@ -1,25 +1,26 @@
 import { Edge, Graph, Node, Path } from "../DataStructures.ts";
 
 export abstract class AlgoAbstract {
-    protected graph: Graph;
+  protected graph: Graph;
 
-    protected constructor(graph: Graph) {
-        this.graph = graph;
+  protected constructor(graph: Graph) {
+    this.graph = graph;
+  }
+
+  // Method to reconstruct the path using backtracking from the endNode
+  protected reconstructPath(
+    cameFrom: Map<Node, Node | null>,
+    currentNode: Node,
+  ): Path {
+    const edges: Edge[] = [];
+    while (cameFrom.has(currentNode)) {
+      const prevNode = cameFrom.get(currentNode)!;
+      const edge = this.graph.getEdge(prevNode, currentNode);
+      if (edge) {
+        edges.unshift(edge);
+      }
+      currentNode = prevNode;
     }
-
-
-    // Method to reconstruct the path using backtracking from the endNode
-    protected reconstructPath(cameFrom: Map<Node, Node | null>, currentNode: Node): Path {
-        const edges: Edge[] = [];
-        while (cameFrom.has(currentNode)) {
-            const prevNode = cameFrom.get(currentNode)!;
-            const edge = this.graph.getEdge(prevNode, currentNode);
-            if (edge) {
-                edges.unshift(edge);
-            }
-            currentNode = prevNode;
-        }
-        return new Path(edges);
-    }
-
+    return new Path(edges);
+  }
 }
