389218fcc35afda6f657d8bb743b894f465c9174
macethewindu66 <madduxberry31@gmail.com>
2024-04-26 13:47:44 -0400

    Added search bar and drop downs

--- apps/frontend/src/routes/EmployeePage.tsx
+++ apps/frontend/src/routes/EmployeePage.tsx
@@ -1,6 +1,29 @@
 import styles from "../styles/EmployeePage.module.css";
+import { SetStateAction, useState } from "react";
+
 //import Button from "@mui/material";
 function EmployeePage() {
+  const [searchTerm, setSearchTerm] = useState(""); // Correctly initialized state
+  const [role, setRoleTerm] = useState("");
+  const [permission, setPermissionTerm] = useState("");
+  const handleSearchChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setSearchTerm(event.target.value); // Updates the state based on input
+  };
+
+  const handleRoleChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setRoleTerm(event.target.value); // Updates the state based on input
+  };
+
+  const handlePermissionChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setPermissionTerm(event.target.value); // Updates the state based on input
+  };
+
   return (
     <>
       <div className={`${styles.pageContainer}`}>
@@ -20,6 +43,35 @@ function EmployeePage() {
 
           <div className={`${styles.filtersSection}`}>
             {/*Here will go the filters and search bar*/}
+            <input
+              type={"text"}
+              placeholder={"Search Employees"}
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className={styles.searchBar}
+            />
+            <div className={`${styles.dropDownFilterBox}`}>
+              <select
+                value={role}
+                onChange={handleRoleChange}
+                className={styles.filterDropDown}
+              >
+                <option value="">Role</option>
+                <option value="Nurse">Nurse</option>
+                <option value="MRI Technician">MRI Technician</option>
+                <option value={"Clerk"}>Clerk</option>
+                <option value={"Doctor"}>Doctor</option>
+              </select>
+              <select
+                value={permission}
+                onChange={handlePermissionChange}
+                className={styles.filterDropDown}
+              >
+                <option value={""}>Permission</option>
+                <option value={"Staff"}>Staff</option>
+                <option value={"Admin"}>Admin</option>
+              </select>
+            </div>
           </div>
 
           <div className={`${styles.tableSection}`}>
