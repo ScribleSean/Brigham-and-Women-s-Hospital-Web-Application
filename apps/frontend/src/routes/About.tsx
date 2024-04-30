d77edbeabd370be144785a5ebfabe45f0f528462
Ethan Glasby <ethan.a.glasby@gmail.com>
2024-04-30 02:50:37 -0400

    Another minor quote update

--- apps/frontend/src/routes/About.tsx
+++ apps/frontend/src/routes/About.tsx
@@ -33,7 +33,7 @@ function About() {
       gamertag: "Lorenzo Cassano",
       description: "Full-Stack Developer",
       quote:
-        '"I visualize a time when we will be to robots what dogs are to humans, and I\'m rooting for the machines." - Claude Shannon',
+        '"In the kingdom of the blind, the one-eyed man is king." - Desiderius Erasmus',
       image: Lorenzo,
     },
     {
